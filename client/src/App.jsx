fe48737a9c883d275ea23b7a487a5f7753b58144
avilamrales <jesusavilamrales@protonmail.com>
2025-05-01 10:00:56 -0600

    feat: initialize frontend with Vite + React and basic setup
    Initialized the client app using Vite and React, with basic setup for styling with tailwindcss and routing with react-ruter.
    Included initial project structure and configuration for future development.

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<h1 className="text-4xl font-bold">Home Page</h1>}
+        />
+        <Route
+          path="/login"
+          element={<h1 className="text-4xl font-bold">Login</h1>}
+        />
+        <Route
+          path="/register"
+          element={<h1 className="text-4xl font-bold">Register</h1>}
+        />
+        <Route
+          path="/tasks"
+          element={<h1 className="text-4xl font-bold">Tasks Page</h1>}
+        />
+        <Route
+          path="/add-task"
+          element={<h1 className="text-4xl font-bold">New Task</h1>}
+        />
+        <Route
+          path="/tasks/:id"
+          element={<h1 className="text-4xl font-bold">Update Task</h1>}
+        />
+        <Route
+          path="/profile"
+          element={<h1 className="text-4xl font-bold">Profile</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
